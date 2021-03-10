25acd35f958c3105d76de1f840616b1b56f900ff
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-03-10 11:06:10 -0800

    👨🏻‍💻 Created Unit Tests for Course swift file

--- gp-aide/Course.swift
+++ gp-aide/Course.swift
@@ -11,8 +11,13 @@ class Course {
     var title: String
     var grade: String
     var credits: Int
+    var printOut: String {
+        var courseStr: String = ""
+        courseStr += "\(title): Grade Received \(grade). Credits for course: \(credits) cr \n"
+        return courseStr
+    }
 
-    init(classTitle: String, classGrade: String, classCredits: Int) {
+    init(_ classTitle: String, classGrade: String, classCredits: Int) {
         self.title = classTitle
         self.grade = classGrade
         self.credits = classCredits
---
+++ gp-aideTests/CourseTests.swift
@@ -0,0 +1,63 @@
+//
+//  CourseTests.swift
+//  gp-aideTests
+//
+//  Created by Samuel Sandoval on 3/10/21.
+//
+
+@testable import gp_aide
+import XCTest
+
+class CourseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCourseInitializer() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        XCTAssertEqual(currCourse.getCourseTitle(), "FirstCourse")
+    }
+
+    func testSetCourseTitle() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse.setCourseTitle("NewCourseTitle")
+        XCTAssertEqual(currCourse.getCourseTitle(), "NewCourseTitle")
+    }
+
+    func testSetCourseGrade() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse.setCourseGrade("B+")
+        XCTAssertEqual(currCourse.getCourseGrade(), "B+")
+    }
+
+    func testSetCourseCredits() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse.setCourseCredit(1)
+        XCTAssertEqual(currCourse.getCourseCredit(), 1)
+    }
+    
+    func testChangeMultipleVariables() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse.setCourseTitle("SecondCourse")
+        currCourse.setCourseGrade("B")
+        currCourse.setCourseCredit(4)
+        XCTAssert(currCourse.printOut == "SecondCourse: Grade Received B. Credits for course: 4 cr \n")
+    }
+
+    func testPrintOut() {
+        let currCourse = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse.setCourseTitle("NewCourseTitle")
+        XCTAssert(currCourse.printOut == "NewCourseTitle: Grade Received A+. Credits for course: 3 cr \n")
+    }
+    
+    func testCourseDeInitializer() {
+        var currCourse: Course? = Course("FirstCourse", classGrade: "A+", classCredits: 3)
+        currCourse = nil
+        XCTAssertEqual(currCourse?.grade, nil)
+    }
+}
