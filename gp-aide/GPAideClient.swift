6a0014791db5562f0631830806be2b60e6ab2d04
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-04-18 21:29:14 -0700

    commit

--- gp-aide/GPAideClient.swift
+++ gp-aide/GPAideClient.swift
@@ -7,50 +7,50 @@
 
 import Foundation
 
-struct Course {
-  var title: String
-  var letterGrade: String
-}
+//struct Course {
+//    var title: String
+//    var letterGrade: String
+//}
 
 struct Classmate {
-  var username: String
-  var contactUrl: String
-  var courses: [Course]
+    var username: String
+    var contactUrl: String
+    var courses: [Course]
 }
 
 struct User {
-  var username: String
-  var contactUrl: String
-  var courses: [Course]
-  var isPublic: Bool
-  var hashedPassword: String
+    var username: String
+    var contactUrl: String
+    var courses: [Course]
+    var isPublic: Bool
+    var hashedPassword: String
 }
 
 struct GPAideResponse {
-  var isSuccessful: Bool
-  var message: String?
+    var isSuccessful: Bool
+    var message: String?
 }
 
 protocol GPAideClient: class {
-  func fetchClassmates(taking courseTitle: String) -> [Classmate]
-  func pullInfo(username: String, password: String) -> User
-  func pushInfo(_ info: User) -> GPAideResponse
-  func discardInfo(_ info: User) -> GPAideResponse
+    func fetchClassmates(taking courseTitle: String) -> [Classmate]
+    func pullInfo(username: String, password: String) -> User
+    func pushInfo(_ info: User) -> GPAideResponse
+    func discardInfo(_ info: User) -> GPAideResponse
 }
 
 func hashPassword(
-  username: String,
-  password: String
+    username: String,
+    password: String
 ) -> String {
-  // TODO(@EthanThatOneKid): Create simple password hashing function.
-  return "\(username).\(password)"
+    // TODO(@EthanThatOneKid): Create simple password hashing function.
+    return "\(username).\(password)"
 }
 
 func getAsciiCodeFromSymbol(from symbol: String) -> Int {
-  let scalar = symbol.unicodeScalars
-  return Int(scalar[scalar.startIndex].value)
+    let scalar = symbol.unicodeScalars
+    return Int(scalar[scalar.startIndex].value)
 }
 
 func getSymbolFromAsciiCode(from asciiCode: Int) -> String {
-  return String(UnicodeScalar(UInt8(asciiCode)))
+    return String(UnicodeScalar(UInt8(asciiCode)))
 }
