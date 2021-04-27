0dcbf85ed8028ede46e2d2412cf5373b30beae25
EthanThatOneKid <ethan.r.davidson@gmail.com>
2021-04-27 12:07:08 -0700

    Fixed broken SimulatedClient tests.

--- gp-aideTests/SimulatedClientTests.swift
+++ gp-aideTests/SimulatedClientTests.swift
@@ -26,46 +26,54 @@ class SimulatedClientTests: XCTestCase {
     
     func testFoundClassmatesByClassOne() {
         let testCourse = "CPSC 223"
-        let foundClassmates = self.client.fetchClassmates(taking: testCourse)
-        XCTAssertEqual(foundClassmates.count, 4)
+        self.client.fetchClassmates(taking: testCourse) { foundClassmates in
+            XCTAssertEqual(foundClassmates.count, 4)
+        }
     }
     
     func testFoundClassmatesByClassTwo() {
         let testCourse = "CPSC 481"
-        let foundClassmates = self.client.fetchClassmates(taking: testCourse)
-        XCTAssertEqual(foundClassmates.count, 2)
+        self.client.fetchClassmates(taking: testCourse) { foundClassmates in
+            XCTAssertEqual(foundClassmates.count, 2)
+        }
     }
     
     func testFoundClassmatesByClassThree() {
         let testCourse = "CPSC 315"
-        let foundClassmates = self.client.fetchClassmates(taking: testCourse)
-        XCTAssertEqual(foundClassmates.count, 1)
+        self.client.fetchClassmates(taking: testCourse) { foundClassmates in
+            XCTAssertEqual(foundClassmates.count, 1)
+        }
     }
     
     func testFoundClassmatesByClassFour() {
         let testCourse = "CPSC 411"
-        let foundClassmates = self.client.fetchClassmates(taking: testCourse)
-        XCTAssertEqual(foundClassmates.count, 1)
+        self.client.fetchClassmates(taking: testCourse) { foundClassmates in
+            XCTAssertEqual(foundClassmates.count, 1)
+        }
     }
 
     func testNoClassmatesMatch() {
-        let classmates = self.client.fetchClassmates(taking: "courseTitle1")
-        XCTAssertEqual(classmates.count, 0)
+        self.client.fetchClassmates(taking: "courseTitle1") { classmates in
+            XCTAssertEqual(classmates.count, 0)
+        }
     }
 
     func testPullInfo() {
-        let info = client.pullInfo(username: TEST_USER.username, password: TEST_USER.hashedPassword)
-        XCTAssertEqual(info, TEST_USER)
+        self.client.pullInfo(username: TEST_USER.username, password: TEST_USER.hashedPassword) { info in
+            XCTAssertEqual(info, TEST_USER)
+        }
     }
 
     func testPushInfo() {
-        let response = client.pushInfo(TEST_USER)
-        XCTAssertTrue(response.isSuccessful)
+        self.client.pushInfo(TEST_USER) { response in
+            XCTAssertTrue(response.isSuccessful)
+        }
     }
 
     func testDiscardInfo() {
-        let response = client.discardInfo(TEST_USER)
-        XCTAssertTrue(response.isSuccessful)
+        self.client.discardInfo(TEST_USER) { response in
+            XCTAssertTrue(response.isSuccessful)
+        }
     }
     
     
